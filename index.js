9db99f3c53c9ba539c85b323589d647737e7d554
Matheus Poleza <matheus.poleza@contaazul.com>
2015-12-11 10:16:57 -0200

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const path = require('path');
-const fs = require('fs');
-
-fs.readFile(
-  path.resolve(__dirname, 'info.json'), 'utf8', 
-  (err, data) => console.log(err || data)
-);
